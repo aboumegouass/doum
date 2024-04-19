b18776ac9cd9eb7b1bd6efbdaf04ef0445833022
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2024-04-19 11:19:57 +0100

    Fixed

--- index.php
+++ index.php
@@ -192,6 +192,186 @@
         <?php endwhile; else :
                     echo 'خطأ'; endif; wp_reset_postdata(); ?>
     </div>
+
+
+
+
+
+
+
+    <div class="page-header">
+        <h2 class="title"><?php echo __( 'أخبار تربوية', 'jdsp' ); ?></h2>
+    </div>
+    <div class="row">
+        <?php
+                    $args = array(
+                        'posts_per_page' => 4,
+                        'order' => 'DESC',
+                        'cat' => '20'
+                    );
+                    $eyemich = new WP_Query($args);
+                    if ($eyemich->have_posts()) :
+                        while ($eyemich->have_posts()) : $eyemich->the_post();
+                        $url = get_the_post_thumbnail_url();
+                ?>
+        <div class="col-sm-6">
+
+            <a href="<?php the_permalink(); ?>" class="flex mb-3 last:mb-0 items-center gap-2">
+                <div class="w-2/5">
+                    <img src="<?php echo $url; ?>" class="w-full object-cover h-44 max-sm:h-28 rounded-md" alt="">
+                </div>
+                <div class="w-3/5">
+                    <div class="post-item-content-inner">
+                        <h4 class="title text-lg font-bold"><?php the_title(); ?></h4>
+                        <p class="text-xs"><?php the_time(); ?></p>
+                        <p class="mt-2 text-sm max-sm:hidden">
+                            <?php 
+                                $excerpt = get_the_excerpt(); 
+                                
+                                $excerpt = substr( $excerpt, 0, 290 ); // Only display first 260 characters of excerpt
+                                $result = substr( $excerpt, 0, strrpos( $excerpt, ' ' ) );
+                                echo $result . " ...";
+                                ?>
+                        </p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <?php endwhile; else :
+                    echo 'خطأ'; endif; wp_reset_postdata(); ?>
+    </div>
+
+
+
+
+
+
+
+
+    <div class="page-header">
+        <h2 class="title"><?php echo __( 'ملتقيات والمجالس الوطنية', 'jdsp' ); ?></h2>
+    </div>
+    <div class="row">
+        <?php
+                    $args = array(
+                        'posts_per_page' => 4,
+                        'order' => 'DESC',
+                        'cat' => '21'
+                    );
+                    $eyemich = new WP_Query($args);
+                    if ($eyemich->have_posts()) :
+                        while ($eyemich->have_posts()) : $eyemich->the_post();
+                        $url = get_the_post_thumbnail_url();
+                ?>
+        <div class="col-sm-3">
+            <a href="<?php the_permalink(); ?>" class="post-item rounded-md">
+                <div class="post-item-img" style="background-image: url(<?php echo $url; ?>)"></div>
+                <div class="post-item-content">
+                    <div class="post-item-content-inner">
+                        <h4 class="title"><?php the_title(); ?></h4>
+                        <p class="meta"><?php the_time(); ?></p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <?php endwhile; else :
+                    echo 'خطأ'; endif; wp_reset_postdata(); ?>
+    </div>
+
+
+
+
+
+    <div class="page-header">
+        <h2 class="title"><?php echo __( 'مواقف ومطالب', 'jdsp' ); ?></h2>
+    </div>
+    <div class="row">
+        <?php
+                    $args = array(
+                        'posts_per_page' => 4,
+                        'order' => 'DESC',
+                        'cat' => '19'
+                    );
+                    $eyemich = new WP_Query($args);
+                    if ($eyemich->have_posts()) :
+                        while ($eyemich->have_posts()) : $eyemich->the_post();
+                        $url = get_the_post_thumbnail_url();
+                ?>
+        <div class="col-sm-6">
+
+            <a href="<?php the_permalink(); ?>" class="flex mb-3 last:mb-0 items-center gap-2">
+                <div class="w-2/5">
+                    <img src="<?php echo $url; ?>" class="w-full object-cover h-44 max-sm:h-28 rounded-md" alt="">
+                </div>
+                <div class="w-3/5">
+                    <div class="post-item-content-inner">
+                        <h4 class="title text-lg font-bold"><?php the_title(); ?></h4>
+                        <p class="text-xs"><?php the_time(); ?></p>
+                        <p class="mt-2 text-sm max-sm:hidden">
+                            <?php 
+                                $excerpt = get_the_excerpt(); 
+                                
+                                $excerpt = substr( $excerpt, 0, 290 ); // Only display first 260 characters of excerpt
+                                $result = substr( $excerpt, 0, strrpos( $excerpt, ' ' ) );
+                                echo $result . " ...";
+                                ?>
+                        </p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <?php endwhile; else :
+                    echo 'خطأ'; endif; wp_reset_postdata(); ?>
+    </div>
+
+
+
+
+
+
+
+
+
+    <div class="page-header">
+        <h2 class="title"><?php echo __( 'صحف ومقالات', 'jdsp' ); ?></h2>
+    </div>
+    <div class="row">
+        <?php
+                    $args = array(
+                        'posts_per_page' => 4,
+                        'order' => 'DESC',
+                        'cat' => '22'
+                    );
+                    $eyemich = new WP_Query($args);
+                    if ($eyemich->have_posts()) :
+                        while ($eyemich->have_posts()) : $eyemich->the_post();
+                        $url = get_the_post_thumbnail_url();
+                ?>
+        <div class="col-sm-3">
+            <a href="<?php the_permalink(); ?>" class="post-item rounded-md">
+                <div class="post-item-img" style="background-image: url(<?php echo $url; ?>)"></div>
+                <div class="post-item-content">
+                    <div class="post-item-content-inner">
+                        <h4 class="title"><?php the_title(); ?></h4>
+                        <p class="meta"><?php the_time(); ?></p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <?php endwhile; else :
+                    echo 'خطأ'; endif; wp_reset_postdata(); ?>
+    </div>
+
+
+
+
+
+
+
+
+
     <div class="row">
         <div class="col-lg-6">
             <div class="page-header">
